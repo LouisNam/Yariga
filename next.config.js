b938fcfe548be0dd6138871cea99b3d5a82d4ad5
Nam Do <dotranthainam@gmail.com>
2023-06-06 00:41:06 +0700

    Update for config image

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["source.unsplash.com", "images.unsplash.com"],
+    formats: ["image/webp"],
+    minimumCacheTTL: 60,
   },
 };
 
